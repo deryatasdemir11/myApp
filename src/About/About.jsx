69015a8940b75d9e5e1ea657d2b4a55e29790267
derya <derya.tasdemir.00@gmail.com>
2023-01-24 13:08:04 +0300

    About completed

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -3,6 +3,17 @@ import './about.css';
 
 export default function About () {
   return (
-    <div>About</div>
+    <div className="about about-header">
+      <h3>Summary</h3>
+      <p>Hey I'm Derya 
+        <li>I graduated from Pamukkale University by finishing Biomedical Enginerring Program.</li>
+        <li>I cultivated my self on image processing and mobile application development.</li>
+        <li>While I'm student i worked on so many projects using image processing.</li>
+        <li>One of them was my thesis it was about lung cancer diagnostic using AI and image processing.</li>
+        <li>On my intern-engineering I worked on React-Native and completed 2 application development projects.</li>
+        <li>I joined TEKNOFEST 2022 Technology Festival.</li>
+        <li>For this Festival i made a required software for a vehicle that works autonomous operation and search for a special plant and then agricultural spray it.</li>
+      </p>
+    </div>
   )
 }
